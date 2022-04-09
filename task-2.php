02f33f12da24433cf688477b2698e896b82ce876
Lee Vladislave <94622207+Leevladislove@users.noreply.github.com>
2022-04-09 17:58:00 +0300

    Add files via upload

---
+++ task-2.php
@@ -0,0 +1,9 @@
+<?php
+
+const DRAWINGS = 80;
+const MARKERS = 23;
+const PENCILS = 40;
+
+const PAINTS = DRAWINGS - (MARKERS + PENCILS);
+print (PAINTS . ' - столько рисунков выполнено красками');
+
