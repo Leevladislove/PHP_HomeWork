d76d3e94a48daf447407ebfa949d7d51d7c4d5e3
Lee Vladislave <94622207+Leevladislove@users.noreply.github.com>
2022-04-10 22:58:33 +0300

    Delete task_2.php

--- DZ_1/task_2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-const DRAWINGS = 80;
-const MARKERS = 23;
-const PENCILS = 40;
-
-const PAINTS = DRAWINGS - (MARKERS + PENCILS);
-print (PAINTS . ' - столько рисунков выполнено красками');
-
